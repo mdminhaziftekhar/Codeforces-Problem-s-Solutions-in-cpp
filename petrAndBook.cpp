3abcffca0ddea004fc9fb6c21495b9693676afcc
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2021-08-07 11:23:18 +0600

    139A - Petr and Book

---
+++ petrAndBook.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long int;
+
+const int MAX_N = 1e5 + 1;
+const ll M = 1e9 + 7;
+const ll INF = 1e9;
+
+//***** Generates Prime till 5 million numbers *****
+// bool is_prime[90000001];
+// vector<ll> primes;
+// void sieve(){
+//     ll maxN = 90000001;
+
+//     for(ll i = 1; i<=maxN; i++) is_prime[i] = 1;
+
+//     is_prime[0] = is_prime[1] = 0;
+
+//     for(ll i = 2; i*i <= maxN; i++){
+//         if(is_prime[i]){
+//             for(ll j = i*i; j <= maxN; j+=i) is_prime[j] = 0;
+//         }
+//     }
+    
+//     for(ll i = 1; i<=maxN; i++){
+//         if(is_prime[i]){
+//             primes.emplace_back(i);
+//         }
+//     }
+// }
+
+//..... simple Implimentation of sieve .....
+// int is_prime[1000001];
+// void sieve(){
+//     int maxN = 1000000;
+
+//     for(int i = 1; i<=maxN; i++) is_prime[i] = 1;
+
+//     is_prime[0] = is_prime[1] = 0;
+
+//     for(int i = 2; (ll)i*i <= maxN; i++){
+//         if(is_prime[i]){
+//             for(int j = i*i; j <= maxN; j+=i) is_prime[j] = 0;
+//         }
+//     }
+
+// }
+
+ll mod(ll x){
+    return ((x%M + M)%M);
+}
+
+ll add(ll a, ll b){
+    return mod(mod(a) + mod(b));
+}
+
+ll mul(ll a, ll b){
+    return mod(mod(a) * mod(b));
+}
+
+//Solution
+void solve() {
+
+    
+}
+
+//Main
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int n;
+
+    cin >> n;
+
+    vector<int> a(7);
+
+    for(int &i : a) cin >> i;
+
+    int i = 0, last = 0;
+
+    while(n > 0){
+
+        n = n - a[i];
+
+        last = i+1;
+
+        i++; 
+
+        if(i == a.size()) i = 0;
+
+        if(n < 0 ) break;
+
+    }
+
+    cout << last << endl;
+
+    return 0;
+}
